db232ec4f83e4c873e0a676a1987ccdb94641046
mistrjirka <jirka.svitil@gmail.com>
2018-10-29 15:50:20 +0100

    timing

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ class Robot():
 		data[0] = self.sonicValue()
 		
 		self.SM.run_to_rel_pos(position_sp=-180, speed_sp=self.SM_speed, stop_action="hold")
-		sleep(self.SM_sleep)
+		sleep(self.SM_sleep*2)
 		data[2] = self.sonicValue()
 		
 		self.SM.run_to_rel_pos(position_sp=90, speed_sp=self.SM_speed, stop_action="hold")
