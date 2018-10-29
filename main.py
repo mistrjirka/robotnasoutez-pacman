6a90f8acb2d99948760a95774749da805cc1efb5
mistrjirka <jirka.svitil@gmail.com>
2018-10-29 11:12:31 +0100

    bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ev3dev.ev3 import UltrasonicSensor, MediumMotor, LargeMotor, TouchSensor, S
 from threading import Thread 
 
 class Robot():
-	def __init__(self, SM, mot1, mot2, GP = None, US = None, SM_speed = 1550, SM_sleep = 0.04, critical_distance = 20, max_map_size = [20,20], turn_tolerance = 0.05, straight_tolerance = 2, motor_speed = 200, motor_speed_turning = 150):
+	def __init__(self, SM, mot1, mot2, GP = None, US = None, SM_speed = 1550, SM_sleep = 0.06, critical_distance = 20, max_map_size = [20,20], turn_tolerance = 0.05, straight_tolerance = 2, motor_speed = 200, motor_speed_turning = 150):
 		#this is intitial configuration
 		if GP == None: #shitty
 			self.TrueTurn = TrueTurn(mot1, mot2)
@@ -138,6 +138,7 @@ class Robot():
 			cache[0] = self.US.value()/10 
 			sleep(0.01)
 			cache[1] = self.US.value()/10
+			sleep(0.01)
 		return sum(cache) / len(cache)
 	
 	def checkWay(self): #async function
