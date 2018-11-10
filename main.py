805bab09eb023d5a991e750c1f72d1c1430816b6
mistrjirka <jirka.svitil@gmail.com>
2018-11-10 20:57:25 +0100

    new lib test

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ class Robot():
 		data[0] = self.sonicValue()
 		
 		self.SM.run_to_rel_pos(position_sp=-180, speed_sp=self.SM_speed, stop_action="hold")
-		sleep(self.SM_sleep*2)
+		sleep(self.SM_sleep*3)
 		data[2] = self.sonicValue()
 		
 		self.SM.run_to_rel_pos(position_sp=90, speed_sp=self.SM_speed, stop_action="hold")
